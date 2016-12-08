ede34f93ed47eb921aec71eecc571cbe3163c664
Stacy Brock <brock@oregonstate.edu>
2016-12-08 14:19:03 -0800

    Move target host config to environment variable
    - Preserve nodes config on target host
    - Add .bashrc to exclude list

--- sync-jenkins-config.sh
+++ sync-jenkins-config.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-TARGET_HOST=changeme
+if [ -z "$SYNC_JENKINS_HOST" ]; then
+  echo "The SYNC_JENKINS_HOST environment variable must be set."
+  exit 1
+fi
 
 # put jenkins into quiet mode
 /opt/scripts/jenkins-job/bin/jenkins-job quiet
 
+# backup nodes dir
+ssh root@$SYNC_JENKINS_HOST "cp -r /var/lib/jenkins/nodes /tmp/"
+
 # clear jobs dir
-ssh root@$TARGET_HOST "rm -rf /var/lib/jenkins/jobs"
+ssh root@$SYNC_JENKINS_HOST "rm -rf /var/lib/jenkins/jobs"
 
 # sync config
-rsync -avz --exclude-from 'rsync_exclude.txt' /var/lib/jenkins root@$TARGET_HOST:/var/lib/
+rsync -avz --exclude-from 'rsync_exclude.txt' /var/lib/jenkins root@$SYNC_JENKINS_HOST:/var/lib/
+
+# restore nodes dir
+ssh root@$SYNC_JENKINS_HOST "cp -r /tmp/nodes /var/lib/jenkins/jobs/"
 
 # reload Jenkins configuration
 /opt/scripts/jenkins-job/bin/jenkins-job reload
